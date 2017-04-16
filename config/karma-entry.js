a589f5c97c09d34092781673384543eff475199e
qiang <qiangonline@gmail.com>
2017-04-16 18:52:20 +0800

    test

--- config/karma-entry.js
+++ config/karma-entry.js
@@ -4,5 +4,5 @@ require('angular');
 require('angular-mocks');
 
 
-let r = require.context('./src', true, /\.spec\.js$/);
+let r = require.context('../src', true, /\.spec\.js$/);
 r.keys().forEach(r);
--- config/karma.conf.js
+++ config/karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
-            path.join(__dirname, '../test-context.js')
+            path.join(__dirname, './karma-entry.js')
         ],
 
 
@@ -29,7 +29,7 @@ module.exports = function (config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            [path.join(__dirname, '../test-context.js')]: ['webpack', 'sourcemap']
+            [path.join(__dirname, './karma-entry.js')]: ['webpack', 'sourcemap']
         },
 
 
